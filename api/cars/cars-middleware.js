915eacee5491bfa5dab397ef60c3f7cb2171640f
gabe-morris <gabe.e.morris@gmail.com>
2022-03-25 12:20:45 -0500

    CarPayload and CarID mw

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,52 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Car = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  try{
+    const car = await Car.getById(req.params.id)
+    if(!car){
+      next({status: 404, message: `car with id ${req.params.id} is not found`})
+    }else{
+      req.car = car
+      next()
+    }
+}catch(err){
+    next(err)
+}
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkCarPayload = async (req, res, next) => {
+  if(!req.body.vin) return next({
+      status:400,
+      message:'vin is missing'
+  })
 
+  if (!req.body.make) return next({
+        status:400,
+        message: 'make is missing'
+  })
+
+  if(!req.body.model) return next({
+        status:400,
+        message: 'model is missing'
+  })
+
+  if(!req.body.mileage) return next({
+        status:400,
+        message: 'mileage is missing'
+      })
+      next()
+}
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
 }
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
 }
